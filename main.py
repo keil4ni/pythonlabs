23db309711c8d6c96329f3ee1587011884964690
Keilani Li <97659645+keil4ni@users.noreply.github.com>
2023-07-05 17:13:30 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+# Keilani Li
+# The following coding program prompts the user for hours and rate per hour to calculate their gross pay
+hours = float(input('Enter Hours: '))
+rate = float(input('Enter Rate: '))
+
+total = hours * rate
+print('\n\nPay: ', total)
+
+'''
+Enter Hours: 35
+Enter Rate: 2.75
+
+
+Pay:  96.25
+
+Process finished with exit code 0
+'''
+
+'''
+Enter Hours: 32
+Enter Rate: 15.50
+
+
+Pay:  496.0
+
+Process finished with exit code 0
+'''
